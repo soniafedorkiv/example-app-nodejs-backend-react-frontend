3d277e35725b617afcb31d461a12ae144ad143f1
soniafedorkiv <soniafedorkiv@gmail.com>
2024-08-26 14:32:37 +0300

    yes
    modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,28 @@
+const path = require('path');
+
 module.exports = {
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				use: {
-					loader: '@sucrase/webpack-loader',
-					options: {
-						transforms: ['jsx']
-					}
-				}
-			}
-		]
-	}
+  mode: 'development', // Змініть на 'production' для продакшн-збірки
+  entry: './src/client/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'static'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: '@sucrase/webpack-loader',
+          options: {
+            transforms: ['jsx'] // Трансформує JSX
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'] // Додає розширення файлів для імпорту
+  },
+  devtool: 'source-map' // Додає мапи для налагодження
 };
